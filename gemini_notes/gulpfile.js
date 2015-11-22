00383533521cc38a6ed38b741d2373c2298934e6
yirikus <izoptera@gmail.com>
2015-11-22 19:34:38 +0100

    Gemini Notes - removed useless gulp tasks and dependencies

--- gemini_notes/gulpfile.js
+++ gemini_notes/gulpfile.js
@@ -1,12 +1,10 @@
 //imports
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var connect = require('gulp-connect');
 var mainBowerFiles = require('main-bower-files');
 var filter = require('gulp-filter');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var livereload = require('gulp-livereload');
 
 
 gulp.task('build', ['js', 'sass', 'html']);
@@ -38,25 +36,4 @@ gulp.task('html', function() {
 gulp.task('sass', function() {
     return sass('sass/style.sass')
         .pipe(gulp.dest('public/css'))
-});
-
-//livereload
-gulp.task('watch', function() {
-    gulp.watch('src/**/*.*', ['build']);
-
-    // Create LiveReload server
-    //livereload.listen();
-
-    // Watch any files in dist/, reload on change
-    //gulp.watch(['public/**']).on('change', livereload.changed);
-
-});
-
-//start the server
-gulp.task('connect', function () {
-    connect.server({
-        root: __dirname +  '/public',
-        port: 9000,
-        livereload: true
-    });
 });
